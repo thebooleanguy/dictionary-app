80f6644508b4ca73bfc6e4ec848ec6a915e815af
thebooleanguy <tharindus0601@gmail.com>
2024-07-27 21:34:45 +0530

    Initialize spring project

---
+++ src/main/java/com/example/dictionary/DictionaryWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dictionary/DictionaryWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
