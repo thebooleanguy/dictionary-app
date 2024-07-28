2c1885fe3e62882f1781b0ab684fbd7229bbc80f
thebooleanguy <tharindus0601@gmail.com>
2024-07-28 13:21:51 +0530

    Change package name and add boilerplate files according to MVC architecture

--- src/main/java/com/thebooleanguy/dictionary/DictionaryWebAppApplication.java
+++ src/main/java/com/thebooleanguy/dictionary/DictionaryWebAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.dictionary;
+package com.thebooleanguy.dictionary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/thebooleanguy/dictionary/DictionaryWebAppApplicationTests.java
+++ src/test/java/com/thebooleanguy/dictionary/DictionaryWebAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.dictionary;
+package com.thebooleanguy.dictionary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
